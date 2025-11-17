aa2b33614dd4f56d1b0bfa89a06df09cd5209fb8
Fondit <bastien.verdiervaissiere@gmail.com>
2025-11-18 00:52:46 +0100

    Fix webpack module resolution for monorepo

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ['media.rawg.io'],
   },
+  webpack: (config) => {
+    // Force resolution depuis la racine du monorepo
+    config.resolve.modules.push(path.resolve(__dirname, '../../node_modules'));
+    return config;
+  },
 };
 
 module.exports = nextConfig;
